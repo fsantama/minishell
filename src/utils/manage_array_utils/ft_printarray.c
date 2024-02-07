0e20732f85f85d49deb1427b87115c7b1565192d
fsantama <fransantamaria95@gmail.com>
2024-02-07 16:44:12 +0100

    ft_printarray comments added

--- src/utils/manage_array_utils/ft_printarray.c
+++ src/utils/manage_array_utils/ft_printarray.c
@@ -6,12 +6,21 @@
 /*   By: fsantama <fsantama@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 10:03:16 by fsantama          #+#    #+#             */
-/*   Updated: 2024/02/05 10:03:17 by fsantama         ###   ########.fr       */
+/*   Updated: 2024/02/07 16:43:37 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
 
+/**
+ * @brief Prints the elements of a string array.
+ * 
+ * This function takes a string array as input and prints each element
+ * of the array to the standard output. If the array is NULL, it prints
+ * a message indicating that the array is NULL.
+ * 
+ * @param arr The string array to be printed.
+ */
 void	ft_printarray(char **arr)
 {
 	if (arr == NULL)
