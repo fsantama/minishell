3906f6323d43eff7b0dd529d5d4d4119cb0b8972
fsantama <fransantamaria95@gmail.com>
2024-02-16 11:35:51 +0100

    añadidas las funciones que añaden la ruta al prompt

--- src/utils/env_utils/ft_basename.c
+++ src/utils/env_utils/ft_basename.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_getline.c                                       :+:      :+:    :+:   */
+/*   ft_basename.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsantama <fsantama@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 10:03:38 by fsantama          #+#    #+#             */
-/*   Updated: 2024/02/16 11:35:06 by fsantama         ###   ########.fr       */
+/*   Created: 2024/02/15 18:12:17 by fsantama          #+#    #+#             */
+/*   Updated: 2024/02/15 18:21:00 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
 
-/**
- *	@brief		Read a user input line with a colored prompt.
- *
- *	@param	str	Additional text to be displayed in the prompt.
- *	@return		char* The user input line, or NULL if an error occurs.
- 	NOTE:		The memory allocated for the string should be freed.
- */
-char	*ft_getline(char *str)
+char	*ft_basename(const char *path)
 {
-	char	*input;
+	const char	*basename;
 
-	input = readline(str);
-	return (free(str), input);
+	basename = ft_strrchr(path, '/');
+	if (basename == NULL)
+		return (ft_strdup(path));
+	return (ft_strdup(basename + 1));
 }
