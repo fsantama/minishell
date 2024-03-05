f9fb334b6d95d829d44b0dbc4b5ea7834e0f62d2
fsantama <fransantamaria95@gmail.com>
2024-03-05 16:54:23 +0100

    ft_checkredirect added

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsantama <fsantama@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 16:39:34 by fsantama          #+#    #+#             */
+/*   Updated: 2024/03/05 16:40:51 by fsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/minishell.h"
 
-void    ft_pwd(void)
+void	ft_pwd(void)
 {
-    char    *path;
+	char	*path;
 
-    path = getcwd(NULL, 0);
-    ft_putendl_fd(path, 1);
-    free(path);
+	path = getcwd(NULL, 0);
+	ft_putendl_fd(path, 1);
+	free(path);
 }
